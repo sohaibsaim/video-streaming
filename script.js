a69adcdc13aee4a0dce40a0112a8c9019e53820f
Sohaib Ali <sohaib.saim.ali@gmail.com>
2023-09-27 23:05:09 +0500

    small fixes

--- script.js
+++ script.js
@@ -20,16 +20,39 @@ function makeLandscape() {
 }
 
 function fullscreen() {
-    var element = document.getElementById('webcamVideo');
-    if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-        makeLandscape();
-        toggleFullScreen();
-    } else if (element.webkitRequestFullScreen) {
-        element.webkitRequestFullScreen();
-        makeLandscape();
-        toggleFullScreen();
+    var elem = document.getElementById('webcamVideo');
+    //# for most browsers
+    if (elem.requestFullscreen) 
+    { elem.requestFullscreen(); } 
+    
+    //# for Safari (older versions)
+    else if (elem.webkitRequestFullscreen) 
+    { elem.webkitRequestFullscreen(); }
+    
+    //# for Safari (newer versions)
+    else if (elem.webkitEnterFullscreen) 
+    { elem.webkitEnterFullscreen(); }
+    
+    //# for Safari iPhone (where only the Video tag itself can be fullscreen)
+    else if (elem.children[0].webkitEnterFullscreen) 
+    { 
+        elem.children[0].webkitEnterFullscreen();
+        //toggle_controls(); //# your own function to show/hide iOS media controls
     }
+    
+    //# for Internet Explorer 11
+    else if (elem.msRequestFullscreen) 
+    { elem.msRequestFullscreen(); }
+    // var element = document.getElementById('webcamVideo');
+    // if (element.mozRequestFullScreen) {
+    //     element.mozRequestFullScreen();
+    //     makeLandscape();
+    //     toggleFullScreen();
+    // } else if (element.webkitRequestFullScreen) {
+    //     element.webkitRequestFullScreen();
+    //     makeLandscape();
+    //     toggleFullScreen();
+    // }
 
 }
 
